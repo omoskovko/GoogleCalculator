b6fb22315a3121c94c9a459c575eec64200529ba
OMoskovko <omoskovko@users.noreply.github.com>
2020-01-30 18:28:25 +0200

    Update yield_example.py

--- example/yield_example.py
+++ example/yield_example.py
@@ -1,32 +1,58 @@
+from inspect import signature
+from collections import OrderedDict
+
 class MyHookimpl(object):
-    def __init__(self, hook_name):
-        self.gen = None
-        self.hook_name = hook_name
+    def __init__(self):
+        self.func_regs = OrderedDict()
 
     def __call__(self, func):
-        print("Hook is invoked")
-        self.gen = func(self.hook_name)
-        val = next(self.gen)
+        parameters = signature(func).parameters
+        params_list = []
+        for p in parameters:
+            if p in self.func_regs:
+               params_list.append(self.func_regs[p][0])
+
+        gen = (func(*params_list))
+        val = next(gen)
+        self.func_regs[func.__name__] = (val, gen)
         return val
 
     def stop_gen(self):
-        try:
           # Here TearDown will be invoked
-          self.gen.send(None)
-          raise Exception("has second yield")
-        except StopIteration:
-          pass
+          err = None
+          for val, g in self.func_regs.values():
+              try:
+                  g.send(None)
+                  err = Exception("{0} has second yield".format(val))
+              except StopIteration:
+                pass
+          if err:
+             raise err
 
-test_wrap = MyHookimpl("Yield example")
+test_wrap = MyHookimpl()
 
 @test_wrap
+def test_name():
+    print("SetUp Basic values")
+    yield "Basic value"
+    print("TearDown Basic values")
+    
+@test_wrap
 def my_test(test_name):
-    print("SetUp {0}".format(test_name))
-    yield "my object is here"
-    print("TearDown {0}".format(test_name))
+    print("SetUp my_test")
+    val = "My test value is: " + test_name
+    yield val
+    print("TearDown my_test")
+    
+@test_wrap
+def my_next_test(test_name, my_test):
+    print("SetUp my_next_test")
+    val = test_name + " - " + my_test
+    yield val
+    print("TearDown my_next_test")
     
 
-print(my_test)
+print(my_next_test)
 
 print("Next step")
 aaa = my_test
@@ -40,11 +66,14 @@ test_wrap.stop_gen()
 '''
 # OutPut of this code will be as following
 
-Hook is invoked
-SetUp Yield example
-my object is here
+SetUp Basic values
+SetUp my_test
+SetUp my_next_test
+Basic value - My test value is: Basic value
 Next step
-aaa=my object is here
+aaa=My test value is: Basic value
 Before teardown
-TearDown Yield example
+TearDown Basic values
+TearDown my_test
+TearDown my_next_test
 '''
