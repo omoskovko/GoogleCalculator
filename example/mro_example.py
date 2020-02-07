84ad459e03caee2e36ee5f6fbdd949e0210df76d
OMoskovko <omoskovko@users.noreply.github.com>
2020-02-07 13:17:30 +0200

    Update mro_example.py

--- example/mro_example.py
+++ example/mro_example.py
@@ -13,12 +13,12 @@ class Example(MyObject):
 class Sibling(MyType):
     pass
 
-class ExampleSibling(Example, Sibling):
+class ExampleSibling(Example, Sibling, NoneSample):
     pass
 
 print(ExampleSibling.__mro__)
 
 '''
 # OutPut is like following
-(<class '__main__.ExampleSibling'>, <class '__main__.Example'>, <class '__main__.MyObject'>, <class '__main__.Sibling'>, <class '__main__.MyType'>, <class 'type'>, <class 'object'>)
+(<class '__main__.ExampleSibling'>, <class '__main__.Example'>, <class '__main__.Sibling'>, <class '__main__.MyType'>, <class 'type'>, <class '__main__.NoneSample'>, <class '__main__.MyObject'>, <class 'object'>)
 '''
