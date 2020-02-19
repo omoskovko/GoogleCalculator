05acc536b50daa00a807405e0939788506ed82ea
OMoskovko <omoskovko@users.noreply.github.com>
2020-02-19 16:03:25 +0200

    Update class_getitem_example.py

--- example/class_getitem_example.py
+++ example/class_getitem_example.py
@@ -6,6 +6,10 @@ class X(object):
         if cls.base == base:
             # makes normal use of class possible
             return cls
+        elif isinstance(base, tuple):
+            name = '{0}[{1}]'.format(cls.__name__, ", ".join([b.__name__ for b in base]))
+            cls_t = base + (cls,)
+            return type(name, cls_t, {'base': base})
         else:
             # construct a new type using the given base class and also remember the attribute for future instantiations
             name = f'{cls.__name__}[{base.__name__}]'
@@ -24,11 +28,16 @@ class X1(X):
 class X2(X):
     def __init__(self):
         # use the attribute here to get the right base for the other subclass
-        self.x1 = X1[self.base]()
+        #self.x1 = X1[self.base]()
+        pass
 
     def f(self):
-        return self.x1.f() + 'X2'
+        return super().f() + 'X2'
 
+
+class X3(X):
+    def f(self):
+        return super().f()+'X3'
 
 # just the wanted new functionality
 class Y(X):
@@ -36,11 +45,11 @@ class Y(X):
         return self.f() + 'Y1'
 
 # demonstration
-y = X2[Y]()
+y = X2[X1, Y]()
 print(type(y).__mro__)
 print(y.g())     # X1X2Y1
 
-x1 = Y[X1]()
+x1 = Y[X3, X1]()
 print(type(x1).__mro__)
 print(x1.g())     # X1Y1
 
