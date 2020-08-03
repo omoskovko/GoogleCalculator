2d1bbfffeadb377b9c3b3f788c107d4f5c2a571e
OMoskovko <omoskovko@users.noreply.github.com>
2020-08-03 13:51:34 +0300

    Update utils.py

--- common/utils.py
+++ common/utils.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-def get_driver(dProfile='Firefox'):
+def get_driver(dProfile='Chrome'):
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
     webDrivers = {}
     webDrivers["Chrome"] = webdriver.Chrome
     webDrivers["Firefox"] = webdriver.Firefox
         
-    return webDrivers.get(dProfile, webdriver.Firefox)()
+    if dProfilr == "Chrome":
+        driver = webDrivers[dProfilr].(chrome_options=options)
+    else:
+        driver = webDrivers[dProfilr]()
+        
+    return driver
