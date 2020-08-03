6b74a29616836de89281a0ff216bc1940054ce44
OMoskovko <omoskovko@users.noreply.github.com>
2020-08-03 09:18:02 +0300

    Add files via upload

---
+++ example/test_params.py
@@ -0,0 +1,25 @@
+# content of test_indirect_list.py
+
+import pytest
+
+
+@pytest.fixture(scope="function")
+def test_x(request):
+    print(f"Param is:{request.param}")
+    return request.param * 3
+
+
+@pytest.fixture(params=[1, 2])
+def test_with_params(request):
+    return request.param
+
+@pytest.fixture(scope="function")
+def y(request, test_with_params):
+    return request.param * 2
+
+
+@pytest.mark.parametrize("test_x, y", [("a", "b")], indirect=["y"])
+def test_indirect(test_x, y):
+    assert y == "b"
+    assert test_x == "aaa"
+
