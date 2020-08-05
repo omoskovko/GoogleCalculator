fe0ae08192425e016b47fa6e12b01d5587f54bcc
OMoskovko <omoskovko@users.noreply.github.com>
2020-08-05 14:24:13 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -48,6 +48,7 @@ def pytest_runtest_makereport(item, call):
 def pytest_addoption(parser):
     parser.addoption("--driver", action="store", default="Firefox", help="WEB Driver name")
     parser.addoption("--testLoops", action="store", default=1, help="Count of suite loops")
+    parser.addoption("--headless", action="store_true", default=False, help="Use headless parameter")
 
 option = None
 
@@ -60,13 +61,17 @@ def pytest_configure(config):
         @pytest.fixture(scope="session", params=[v for v in range(int(config.option.testLoops))])
         def test_loops(self, request):
             return request.param
+        
+        @pytest.fixture(scope="session")
+        def get_headless(self, request):
+            return config.option.headless        
 
     config.pluginmanager.register(Plugin())
 
 @pytest.fixture(scope="session")
-def resource_handler(request):
+def resource_handler(request, get_headless):
     
-    googleBox = GoogleOneBox(get_driver(request.config.option.driver), 'https://www.google.com')
+    googleBox = GoogleOneBox(get_driver(get_headless, request.config.option.driver), 'https://www.google.com')
     rh = googleBox.search_for("1+2=")
 
     # The current best practice for setup and teardown is to use yield
