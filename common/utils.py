bacbb44c9ddc6891f6cd0c831f98ff7703784fef
OMoskovko <omoskovko@users.noreply.github.com>
2020-08-05 14:20:29 +0300

    Update utils.py

--- common/utils.py
+++ common/utils.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 
-def get_driver(dProfile='Chrome'):
+def get_driver(use_headless, dProfile='Chrome'):
     webDrivers = {}
     webDrivers["Chrome"] = webdriver.Chrome
     webDrivers["Firefox"] = webdriver.Firefox
@@ -9,12 +9,14 @@ def get_driver(dProfile='Chrome'):
     if dProfile == "Chrome":
         from selenium.webdriver.chrome.options import Options
         options = Options()
-        options.add_argument('--headless')
+        if use_headless:
+            options.add_argument('--headless')
+            
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
     elif dProfile == "Firefox":
         from selenium.webdriver.firefox.options import Options
         options = Options()
-        options.headless = True
+        options.headless = use_headless
         
     return webDrivers[dProfile](options=options)
